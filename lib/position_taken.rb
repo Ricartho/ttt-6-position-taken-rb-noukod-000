e7074ab2f9c327c2d099bfd87f0d497917740bc9
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-04 18:36:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,9 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0 
 
 def position_taken?(board,index) 
-  if board[index] == " "
+  if board[index] == "X" || board[index] == "O"
+    true
+  elsif board[index] == " "
     false
   elsif board[index] == ""
     false
